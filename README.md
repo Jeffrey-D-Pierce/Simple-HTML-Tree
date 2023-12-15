497a313567146156f32424111880d6adb94d70cf
Jeff Pierce <jeffrey.d.pierce@gmail.com>
2023-12-14 21:41:42 -0500

    Drag and at scale

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ## Expectations
 
-The items data minimal shape requires a unique id, and children with unique id:
+### Item Data
+
+The item data requires a unique id, and children with unique id. All other data is optional.
 
 ```JSON
   {
-    "title": "Template variable",
+    "title": "Template variable", // This is the default label when there is no customContent, otherwise the id will display
     "id": 1, // Must be unique
     "notes": [], // Template variable
     "faviconUrl": "Template variable",
@@ -16,12 +18,36 @@ The items data minimal shape requires a unique id, and children with unique id:
   }
 ```
 
+### Custom Content (customContent)
+
+If there is no customContent function on the item, the title (if there is one) or id will display in the rectangle.
+
+If there is a customContent function, it will be called with the itemInfo to render the item.
+
+The function customContent is set at the root item and inherited by all children. In the rendering of children, the customContent function is copied to the child along with a reference to the parent. The function customContent has access to the child and parent info.
+
 ## Realizations
 
 ### Nodes and Edges are specific to graphs
 
-Hierarchical is a controller-to-controlled relationship such as parent-child or supervisor-subordinate.
+Hierarchical data has a controller-to-controlled relationship such as parent-child or supervisor-subordinate.
 
 ### Horizontal vs. Vertical vs. Centered is controlled by the parent positioning the children
 
+After coding this layout:
+![Alt text](./documentation/images/horizontal_layout.png)
+
+The CSS broke and this image is what triggered the realization.
+
 ![Alt text](./documentation/images/vertical_vanilla.png)
+
+### Rendering of relationships is controlled by sibling type
+
+If an item has childeren, there is a line from the parent.
+All childern have a line to toward the parent.
+The connecting line is determined by the sibling order:
+
+- only-child - has no connecting line to other siblings.
+- first-child - the connecting line starts at the line to the parent and extends toward siblings.
+- middle-child - the connecting line extends toward both the first-child and the last-child
+- last-child - the conntecting line starts at the line toward the parent and extends toward siblings.
--- src/main.js
+++ src/main.js
@@ -52,6 +52,7 @@ function displayItems() {
   items.forEach((item) => {
     // Set the content rendering function for the entity and its childern
     item.customContent = customContent;
+    item.rootItem = true
     nodesHTML += Entity(item);
   });
   return nodesHTML;
@@ -66,3 +67,109 @@ function renderMap(element, version){
 }
 
 if (window) window.renderMap = renderMap
+
+
+let draggableElement = document.getElementById('root');
+let isDragging = false;
+let originalMouseX, originalMouseY;
+
+draggableElement.addEventListener('mousedown', function(e) {
+    isDragging = true;
+    //console.log(draggableElement.style)
+    let orginalTranslation = getTranslateValues(draggableElement.style.transform);
+    originalMouseX = e.clientX - orginalTranslation.x;
+    originalMouseY = e.clientY - orginalTranslation.y;
+    console.log('originalMouseX', originalMouseX, 'originalMouseY', originalMouseY)
+});
+
+document.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+      let scaleValue = getScaleValue(draggableElement.style.transform);
+      console.log('clientX', e.clientX, 'clientY', e.clientY)
+      let deltaX = (e.clientX - originalMouseX) / scaleValue;
+      let deltaY = (e.clientY  - originalMouseY) / scaleValue;
+      console.log('deltaX', deltaX, 'deltaY', deltaY)
+      // draggableElement.style.transform = `translate(${originalX + deltaX}px, ${originalY + deltaY}px)`;
+      updateTranslation(draggableElement, deltaX, deltaY)
+    }
+});
+
+document.addEventListener('mouseup', function(e) {
+    if (isDragging) {
+        isDragging = false;
+    }
+});
+
+
+// Select the slider and the value display element
+const slider = document.getElementById('scale-slider');
+const sliderValueDisplay = document.getElementById('slider-value');
+
+// Add an event listener for the 'input' event on the slider
+slider.addEventListener('input', function() {
+  // Update the display with the current value of the slider
+  sliderValueDisplay.textContent = 'Current Value: ' + this.value;
+  // Select the root element
+  var rootElement = document.getElementById('root'); // replace 'root' with your actual root element's ID
+
+  // Set the scale
+  var scaleValue = this.value / 100; // for 80% scale
+
+  // Apply the transform style to scale
+  // rootElement.style.transform = 'scale(' + scaleValue + ')';
+  updateScale(rootElement, scaleValue)
+  rootElement.style.transformOrigin = 'top left'; // This sets the origin point of the scale transformation
+
+});
+
+
+function updateScale(element, newScale) {
+  let transform = element.style.transform;
+  let scaleRegex = /scale\(\d*\.?\d+\)/;
+  let newScaleTransform = `scale(${newScale})`;
+
+  if (transform.match(scaleRegex)) {
+    element.style.transform = transform.replace(scaleRegex, newScaleTransform);
+  } else {
+    element.style.transform += ` ${newScaleTransform}`;
+  }
+}
+
+function updateTranslation(element, newX, newY) {
+  let scaleValue = getScaleValue(element.style.transform);
+  let transform = element.style.transform;
+  let adjustedX = newX * scaleValue;
+  let adjustedY = newY * scaleValue;
+  let translateRegex = /translate\(-?\d*\.?\d+px,\s*-?\d*\.?\d+px\)/g;
+  let newTranslateTransform = `translate(${adjustedX}px, ${adjustedY}px)`;
+
+  if (transform.match(translateRegex)) {
+    element.style.transform = transform.replace(translateRegex, newTranslateTransform);
+  } else {
+    element.style.transform += ` ${newTranslateTransform}`;
+  }
+}
+
+
+function getScaleValue(transformStyle) {
+  // Regular expression to match the scale value
+  let scaleRegex = /scale\((-?\d*\.?\d+)\)/;
+  let defaultScale = 1; // Default scale (100%)
+
+  // Search for the scale value
+  let matches = scaleRegex.exec(transformStyle);
+
+  // If a scale value is found, parse it to a float, otherwise default to 1
+  let scaleValue = matches ? parseFloat(matches[1]) : defaultScale;
+  // console.log('transformStyle', transformStyle, scaleValue)
+
+  return scaleValue;
+}
+
+// Extract the translate values from the transform string
+function getTranslateValues(transformString) {
+  let translateRegex = /translate\((-?\d+\.?\d*)px,\s*(-?\d+\.?\d*)px\)/;
+  let result = translateRegex.exec(transformString);
+  // console.log('transformString', transformString, result)
+  return result ? { x: parseFloat(result[1]), y: parseFloat(result[2]) } : { x: 0, y: 0 };
+}
